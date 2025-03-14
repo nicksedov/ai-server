0631651956d6177a3524bf77b10ec95efcc0b23a
Nikolay Sedov <nicksedov@yandex.ru>
2025-03-14 23:06:25 +0300

    start.sh

--- start.sh
+++ start.sh
@@ -5,6 +5,3 @@ VENV_HOME=/srv/dev-disk-by-uuid-4315ace7-b3ba-428c-bdf1-bd9b23f4035c/venv
 source $VENV_HOME/bin/activate
 pip install -r requirements.txt
 python main.py --config config-default.yaml
-
-uvicorn main:app
-
