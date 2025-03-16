fdedd99416824de851c3fbcb9ecf8eb29beee6d2
Nikolay Sedov <nicksedov@yandex.ru>
2025-03-16 21:23:56 +0300

    test deleted

--- tests/ai-server/test_completions.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-curl http://localhost:8000/v1/chat/completions \
--H "Content-Type: application/json" \
--d @../prompts/simple.json
--- tests/ai-server/test_image_generations.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-curl -X POST \
- -H 'Content-Type: application/json' \
- -H 'Content-Language: ru' \
- -d @../prompts/image_generation.json \
- http://localhost:8000/v1/images/generations
